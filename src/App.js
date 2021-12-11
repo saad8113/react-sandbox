e5996603b4c6a503b231b612cac4f19c5555bf0d
Saad UR Rehman <saad@getoutsourced.io>
2021-12-11 19:07:27 +0500

    added banner image component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import BannerImage from "./components/banner-image";
 import NavBar from "./components/nav-bar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <BannerImage />
     </div>
   );
 }
